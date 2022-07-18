542550fe33368f1de00a7361a4cd2869d9f3c90f
scriptobi <scriptobi@users.noreply.github.com>
2022-07-19 01:04:51 +0200

    client update

--- client_udp.py
+++ client_udp.py
@@ -3,6 +3,8 @@ import socket
 serverIP = input("Adresse IP du server: ")
 serverPort = 5000
 server = (serverIP, serverPort)
+hostname = socket.gethostname()
+localIP = socket.gethostbyname(hostname)
 msg2Server = "A message from client: "
 toSend = str.encode(msg2Server)
 bufferSize = 1024
@@ -12,10 +14,14 @@ clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     kb_entry = input("Message: ")
-    toSend = str.encode(kb_entry)
-
-    # send to server
-    clientSocket.sendto(toSend, server)
-    msgFromServer = clientSocket.recvfrom(bufferSize)
-    msg = "Message from Server {}".format(msgFromServer[0])
-    print(msg)
+    if not kb_entry:
+        toSend = str.encode("Deconnection du client " + localIP)
+        clientSocket.sendto(toSend, server)
+        break
+    else:
+        toSend = str.encode(kb_entry)
+        # send to server
+        clientSocket.sendto(toSend, server)
+        msgFromServer = clientSocket.recvfrom(bufferSize)
+        msg = "Message from Server {}".format(msgFromServer[0])
+        print(msg)
