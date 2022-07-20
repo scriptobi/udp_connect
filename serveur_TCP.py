b8341e8e53b6dfe69b1a8a7179e0de050a5567ef
scriptobi <scriptobi@yahoo.fr>
2022-07-20 20:35:19 +0200

    tcp server done

--- serveur_TCP.py
+++ serveur_TCP.py
@@ -8,14 +8,19 @@ localPort = 6000
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bind to address and ip
 server.bind((localIP, localPort))
+server.listen(1)
 print(f'TCP server is up and listening on {localIP}.')
 
 # listening for incomming client
 client, clientAddr = server.accept()
 print(f'Connection : {clientAddr}')
-server.send(str.encode('You are connected to the server.'))
-
-while True:
-    data = server.recv(1024)
+client.send(str.encode('You are connected to the server.'))
+cnx = True
+while cnx:
+    data = client.recv(1024)
     print(data)
-    #msg = data.decode(data)
+    if not data:
+        cnx = False
+
+client.close()
+server.close()
